1db70e411e2f434d38f50f53b1464a943e90226a
KillerOfPie <nathan@thors.org>
2023-11-23 10:32:17 -0800

    Initial commit

---
+++ src/main/java/com/pastrymakerstudios/scfws/scfs/SpigotCloudflaredForWebServers.java
@@ -0,0 +1,17 @@
+package com.pastrymakerstudios.scfws.scfs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotCloudflaredForWebServers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
